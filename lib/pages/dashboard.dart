76f70ce0a01ea4cd213ab1579d45d2a7976e678b
Lukas <lukas.wickenhauser@objectbay.com>
2018-12-03 14:25:30 +0100

    loading temperature for sensors on Dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -3,6 +3,7 @@ import 'package:pooltemp_flutter/components/card.dart';
 import 'package:pooltemp_flutter/components/temperatureCard.dart';
 import 'package:pooltemp_flutter/model/temperature.dart';
 import 'package:pooltemp_flutter/pages/temperatureDetails.dart';
+import 'package:pooltemp_flutter/service/temperatureService.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -10,45 +11,56 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-
-  List<Temperature> _temperatures=new List();
+  List<Temperature> _temperatures = new List();
+  TemperatureService _service = TemperatureService();
 
   @override
   void initState() {
-    _temperatures.add(new Temperature(sensorID: "28-80000026d871", temperature: 12.0));
-    _temperatures.add(new Temperature(sensorID: "28-0316a34e6dff", temperature: 23.0));
+    loadTemperatureForSensors();
   }
 
-  void relaod(){
-    setState(() {
-      _temperatures[0].temperature++;
-      _temperatures[1].temperature++;
+  void loadTemperatureForSensors() {
+    _temperatures.clear();
+    _service.findActualTemperatureForSensor("28-80000026d871").then((temperature) {
+      setState(() {
+        _temperatures.add(temperature);
+      });
+    });
+
+    _service.findActualTemperatureForSensor("28-0316a34e6dff").then((temperature) {
+      setState(() {
+        _temperatures.add(temperature);
+      });
     });
   }
 
-  void navigateToDetails(BuildContext context, Temperature temperature){
-   // Navigator.push(context, MaterialPageRoute(builder: (context) => TemperatureDetails(),));
+  void relaod() {
+    loadTemperatureForSensors();
+  }
 
+  void navigateToDetails(BuildContext context, Temperature temperature) {
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => TemperatureDetails(),));
 
     //this is a navigation with a simple slide transition the one above works just fine, but has no transition
-    Navigator.push(context, PageRouteBuilder(pageBuilder: (context,Animation<double> animation,Animation<double>secondaryAnimation){
-      return TemperatureDetails(temperature.temperature, temperature.sensorID);
-    },
-    transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child){
-      return SlideTransition(
-        position: new Tween<Offset>(
-          begin: const Offset(1.0, 0.0),
-          end: Offset.zero,
-        ).animate(animation),
-        child: new SlideTransition(
-          position: new Tween<Offset>(
-            begin: Offset.zero,
-            end: const Offset(1.0, 0.0),
-          ).animate(secondaryAnimation),
-          child: child,
-        ),
-      );
-    }));
+    Navigator.push(
+        context,
+        PageRouteBuilder(pageBuilder: (context, Animation<double> animation, Animation<double> secondaryAnimation) {
+          return TemperatureDetails(temperature.temperature, temperature.sensorID);
+        }, transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+          return SlideTransition(
+            position: new Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: new SlideTransition(
+              position: new Tween<Offset>(
+                begin: Offset.zero,
+                end: const Offset(1.0, 0.0),
+              ).animate(secondaryAnimation),
+              child: child,
+            ),
+          );
+        }));
   }
 
   @override
@@ -63,14 +75,17 @@ class _DashBoardState extends State<DashBoard> {
         color: Colors.blueAccent,
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: _temperatures.map((t)=>Container(
-            child:InkWell(child: TemperatureCard(t),onTap:() => navigateToDetails(context, t),),
-            margin: EdgeInsets.only(top: 5),
-          )).toList(),
+          children: _temperatures
+              .map((t) => Container(
+                    child: InkWell(
+                      child: TemperatureCard(t),
+                      onTap: () => navigateToDetails(context, t),
+                    ),
+                    margin: EdgeInsets.only(top: 5),
+                  ))
+              .toList(),
         ),
       ),
     );
   }
-
-
 }
