6f61443cddfeed005ed2267f6847353ffbaa996d
Lukas <lukas.wickenhauser@objectbay.com>
2019-04-26 14:47:39 +0200

    made LineChart shrinkable for Y Axis

--- lib/components/LineChart.dart
+++ lib/components/LineChart.dart
@@ -5,10 +5,11 @@ import 'package:flutter/widgets.dart';
 class LineChart extends StatefulWidget {
   bool isZoomable;
   bool showXAxis;
+  bool shrinkYAxis;
   List series;
   var onChangeListener;
 
-  LineChart({this.isZoomable, this.series, this.onChangeListener, this.showXAxis: true});
+  LineChart({this.isZoomable, this.series, this.onChangeListener, this.showXAxis: true, this.shrinkYAxis: false});
 
   @override
   _LineChartState createState() => _LineChartState();
@@ -19,12 +20,21 @@ class _LineChartState extends State<LineChart> {
   Widget build(BuildContext context) {
     return charts.TimeSeriesChart(
       widget.series,
+      primaryMeasureAxis: buildAxisSpec(),
       domainAxis: widget.showXAxis ? null : charts.DateTimeAxisSpec(showAxisLine: false, tickProviderSpec: charts.StaticDateTimeTickProviderSpec([])),
       behaviors: createBehavior(),
       dateTimeFactory: charts.LocalDateTimeFactory(),
       animate: true,
       selectionModels: []..addAll(widget.onChangeListener != null ? [charts.SelectionModelConfig(type: charts.SelectionModelType.info, changedListener: widget.onChangeListener)] : []),
     );
+  }
+
+  charts.AxisSpec buildAxisSpec() {
+    if(widget.shrinkYAxis){
+      return charts.AxisSpec(tickProviderSpec: charts.NumericEndPointsTickProviderSpec());
+    }
+    return null;
+
   }
 
   List<charts.ChartBehavior<ChartBehavior>> createBehavior() {
